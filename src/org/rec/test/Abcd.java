b79a5593ed4c4fe1fddeed25d3d9c783c0e080fc
Ramyakishor <rmkishore36@gmail.com>
2023-10-24 15:58:27 +0800

    modified by kishore

--- src/org/rec/test/Abcd.java
+++ src/org/rec/test/Abcd.java
@@ -2,7 +2,8 @@ package org.rec.test;
 
 public class Abcd {
 	public static void main(String[] args) {
-System.out.println("This is demo");		
+    System.out.println("This is demo");		
+    System.out.println("This is modified by Kishore");	
 	}
 
 }
